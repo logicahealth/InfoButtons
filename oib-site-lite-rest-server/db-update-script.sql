69cb941d4b3841f608227e83a3c94b18f66fcadf
Andrew Iskander <andrew.iskander@utah.edu>
2014-12-16 13:34:02 -0700

    Adding creation of utf8 view

--- oib-site-lite-rest-server/db-update-script.sql
+++ oib-site-lite-rest-server/db-update-script.sql
@@ -1,5 +1,7 @@
 ALTER TABLE `profilesdbprod`.`resource_profiles` ADD COLUMN `image_url` VARCHAR(255) NULL AFTER `content`;
 
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `profilesdbprod`.`resource_profiles_utf8` AS select `profilesdbprod`.`resource_profiles`.`id` AS `id`,`profilesdbprod`.`resource_profiles`.`name` AS `name`,`profilesdbprod`.`resource_profiles`.`version` AS `version`,`profilesdbprod`.`resource_profiles`.`published` AS `published`,`profilesdbprod`.`resource_profiles`.`image_url` AS `image_url`,`profilesdbprod`.`resource_profiles`.`status` AS `status`,if((`profilesdbprod`.`resource_profiles`.`status` = 1),'Active',if((`profilesdbprod`.`resource_profiles`.`status` = 2),'Testing','Inactive')) AS `status_dsc`,convert(`profilesdbprod`.`resource_profiles`.`content` using utf8) AS `content_utf8` from `profilesdbprod`.`resource_profiles`;
+
 alter table profilesdbprod.resource_profiles change version version varchar(45);
 
 CREATE TABLE resource_profiles_cloud
