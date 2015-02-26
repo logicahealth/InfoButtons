d54a876c7e21ba82930c960d1008b9b0cecb915d
Andrew Iskander <andrew.iskander@utah.edu>
2015-02-26 10:24:13 -0700

    Fixed create function to use current timestamp

--- oib-site-lite-rest-server/server.js
+++ oib-site-lite-rest-server/server.js
@@ -90,13 +90,24 @@ function getProfile(req,res) {
 
 }
 
+function twoDigits(d) {
+    if(0 <= d && d < 10) return "0" + d.toString();
+    if(-10 < d && d < 0) return "-0" + (-1*d).toString();
+    return d.toString();
+}
+
+Date.prototype.toMysqlFormat = function() {
+    return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) + "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
+};
+
+
 function createProfile(req,res) {
 
 	console.log("Creating new profile: " + req.body.name);
 
 	profileConnectionPool.getConnection( function(err,connection) {
 
-		var newRecord = {name:req.body.name, version:'1', status:'2', content:req.body.content_utf8, published:req.body.version};
+		var newRecord = {name:req.body.name, version:'1', status:'2', content:req.body.content_utf8, published: new Date().toMysqlFormat()};
 		
 		if (req.body.image_url) {
 			newRecord.image_url = req.body.image_url;
