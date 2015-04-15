89967c5c30488cf92f419b127912bf787317d4e7
Andrew <andrew.iskander@utah.edu>
2015-04-15 17:50:31 -0600

    Modify OID matching to match againt sub-OIDs.

--- oib-request/oib-request-service/src/main/java/org/openinfobutton/service/matching/AccessCheckHandler.java
+++ oib-request/oib-request-service/src/main/java/org/openinfobutton/service/matching/AccessCheckHandler.java
@@ -100,7 +100,7 @@ public final class AccessCheckHandler
         for ( int x = 0; x < authCount; x++ )
         {
             id = authorizedOrganizations.get( x );
-            if ( accessID.equals( id.getId() ) )
+            if ( id.getId().contains(accessID) )
             {
 
                 match = true;
