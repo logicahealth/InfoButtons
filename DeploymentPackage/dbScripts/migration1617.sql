60c6075fa86de5b8db262ea020d04cbb9f8c1922
Andrew Iskander <andrew.iskander@utah.edu>
2015-09-18 09:40:59 -0600

    Removing utf8 views from database scripts

--- DeploymentPackage/dbScripts/migration1617.sql
+++ DeploymentPackage/dbScripts/migration1617.sql
@@ -4,8 +4,6 @@ RENAME TABLE resource_profiles TO custom_profiles;
 
 ALTER TABLE `custom_profiles` ADD COLUMN `image_url` VARCHAR(255) NULL AFTER `content`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_custom_profiles` AS select `custom_profiles`.`id` AS `id`,`custom_profiles`.`name` AS `name`,`custom_profiles`.`version` AS `version`,`custom_profiles`.`published` AS `published`,`custom_profiles`.`image_url` AS `image_url`,`custom_profiles`.`status` AS `status`,if((`custom_profiles`.`status` = 1),'Active',if((`custom_profiles`.`status` = 2),'Testing','Inactive')) AS `status_dsc`,convert(`custom_profiles`.`content` using utf8) AS `content_utf8` from `custom_profiles`;
-
 alter table custom_profiles change version varchar(45);
 
 CREATE TABLE installed_store_profiles
@@ -19,8 +17,6 @@ CREATE TABLE installed_store_profiles
   image_url VARCHAR(255),
   PRIMARY KEY (id, version)
 );
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `v_installed_store_profiles` AS select `installed_store_profiles`.`id` AS `id`,`installed_store_profiles`.`name` AS `name`,`installed_store_profiles`.`version` AS `version`,`installed_store_profiles`.`published` AS `published`,`installed_store_profiles`.`image_url` AS `image_url`,`installed_store_profiles`.`status` AS `status`,if((`installed_store_profiles`.`status` = 1),'Active',if((`installed_store_profiles`.`status` = 2),'Testing','Inactive')) AS `status_dsc`,convert(`installed_store_profiles`.`content` using utf8) AS `content_utf8` from `installed_store_profiles`;
 
 create view resource_profiles_all as
   select * from custom_profiles
--- DeploymentPackage/dbScripts/resource_profile.sql
+++ DeploymentPackage/dbScripts/resource_profile.sql
@@ -32,12 +32,6 @@ CREATE TABLE IF NOT EXISTS `custom_profiles` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 --
--- UTF8 custom profiles view
---
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `resource_profile`.`v_custom_profiles` AS select `resource_profile`.`custom_profiles`.`id` AS `id`,`resource_profile`.`custom_profiles`.`name` AS `name`,`resource_profile`.`custom_profiles`.`version` AS `version`,`resource_profile`.`custom_profiles`.`published` AS `published`,`resource_profile`.`custom_profiles`.`image_url` AS `image_url`,`resource_profile`.`custom_profiles`.`status` AS `status`,if((`resource_profile`.`custom_profiles`.`status` = 1),'Active',if((`resource_profile`.`custom_profiles`.`status` = 2),'Testing','Inactive')) AS `status_dsc`,convert(`resource_profile`.`custom_profiles`.`content` using utf8) AS `content_utf8` from `resource_profile`.`custom_profiles`;
-
---
 -- Table structure for table `custom_profiles`
 --
 
@@ -52,13 +46,6 @@ CREATE TABLE IF NOT EXISTS `installed_store_profiles` (
   `image_url` VARCHAR(255),
   PRIMARY KEY (`id`,`version`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
---
--- UTF8 store profiles view
---
-
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `resource_profile`.`v_installed_store_profiles` AS select `resource_profile`.`installed_store_profiles`.`id` AS `id`,`resource_profile`.`installed_store_profiles`.`name` AS `name`,`resource_profile`.`installed_store_profiles`.`version` AS `version`,`resource_profile`.`installed_store_profiles`.`published` AS `published`,`resource_profile`.`installed_store_profiles`.`image_url` AS `image_url`,`resource_profile`.`installed_store_profiles`.`status` AS `status`,if((`resource_profile`.`installed_store_profiles`.`status` = 1),'Active',if((`resource_profile`.`installed_store_profiles`.`status` = 2),'Testing','Inactive')) AS `status_dsc`,convert(`resource_profile`.`installed_store_profiles`.`content` using utf8) AS `content_utf8` from `resource_profile`.`installed_store_profiles`;
-
 --
 -- Combined profile view
 --
