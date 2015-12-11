d43493eb41c43fa50573c752128af8097255df27
Andrew Iskander <shrikeg@gmail.com>
2015-12-11 09:38:07 -0700

    Adding testing script and updated Spring libraries

--- oib-request/oib-service-test/src/test/java/org/openinfobutton/service/fixture/OibRequestTestingFixture.java
+++ oib-request/oib-service-test/src/test/java/org/openinfobutton/service/fixture/OibRequestTestingFixture.java
@@ -1,7 +1,31 @@
 package org.openinfobutton.service.fixture;
 
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
 /**
  * Created by andrew on 9/3/15.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
 public abstract class OibRequestTestingFixture {
+
+    @Configuration
+    @PropertySource("classpath:application.properties")
+    public static class testContext {
+
+        @Bean
+        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+            return new PropertySourcesPlaceholderConfigurer();
+        }
+    }
+
 }
--- oib-request/oib-service-test/src/test/java/org/openinfobutton/service/request/OibLiveServiceTest.java
+++ oib-request/oib-service-test/src/test/java/org/openinfobutton/service/request/OibLiveServiceTest.java
@@ -1,9 +1,52 @@
 package org.openinfobutton.service.request;
 
+import org.junit.Test;
 import org.openinfobutton.service.fixture.OibRequestTestingFixture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 /**
  * Created by andrew on 9/3/15.
  */
 public class OibLiveServiceTest extends OibRequestTestingFixture{
+
+    @Autowired
+    private Environment env;
+
+    @Test
+    public void testServiceAlive() {
+
+        String OIBHtml = new RestTemplate()
+                .getForObject(
+                        env.getProperty("infobutton.location") + "infoRequest?", String.class);
+
+        assertThat(OIBHtml, containsString("OpenInfobutton"));
+    }
+
+    @Test
+    public void testProfileRequest() {
+
+        String OIBHtml = new RestTemplate()
+                .getForObject(
+                        env.getProperty("infobutton.location") + "infoRequest?" + env.getProperty("infobutton.profileQuery"), String.class);
+
+        assertThat(OIBHtml, containsString("UpToDate"));
+    }
+
+    @Test
+    public void testTerminologyRequest() {
+
+        String OIBHtml = new RestTemplate()
+                .getForObject(
+                        env.getProperty("infobutton.location") + "infoRequest?" + env.getProperty("infobutton.terminologyQuery"), String.class);
+
+        assertThat(OIBHtml, containsString("mainSearchCriteria.v.c=250"));
+    }
 }
