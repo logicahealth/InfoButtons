6412825f5a681dc5201fda37b9710d2692bf0f12
Andrew Iskander <andrew.iskander@utah.edu>
2016-03-21 10:28:46 -0600

    Reducing TGT refresh time to 10 minutes until I understand why longer periods fail

--- rest-terminology-services/rest-impl/src/main/java/org/openinfobutton/rest/terminology/impl/UmlsRestClientImpl.java
+++ rest-terminology-services/rest-impl/src/main/java/org/openinfobutton/rest/terminology/impl/UmlsRestClientImpl.java
@@ -173,7 +173,7 @@ public class UmlsRestClientImpl implements RestTermClient {
      */
     private void getTicketGrantingTicket()
     {
-        if (lastUpdate + 10800000000000l < System.nanoTime()) {
+        if (lastUpdate + 600000000000l < System.nanoTime()) {
             logger.error("GETTING TGT");
             try {
                 ticketGrantingTicketURL = Request.Post(UMLS_AUTH_API_URL)
