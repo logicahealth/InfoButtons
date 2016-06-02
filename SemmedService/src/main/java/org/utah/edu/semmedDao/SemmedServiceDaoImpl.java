19b75d604625bcdee9464c6e72fca1d22438ba18
JoeNarus <joe.narus2@gmail.com>
2016-06-02 12:34:03 -0600

    Spring/Hibernate should be set up correctly now.

--- SemmedService/src/main/java/org/utah/edu/semmedDao/SemmedServiceDaoImpl.java
+++ SemmedService/src/main/java/org/utah/edu/semmedDao/SemmedServiceDaoImpl.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -32,58 +33,14 @@ public class SemmedServiceDaoImpl implements SemmedServiceDao {
     @Autowired
     SessionFactory sessionFactory;
 
-    public SemmedServiceDaoImpl() {
-
-    }
-
     @Transactional
     public List<RecentCitationsEntity> getCitations(List<String> PMIDs) {
-        @SuppressWarnings("unchecked")
-        List<RecentCitationsEntity> listUser = (List<RecentCitationsEntity>) sessionFactory.getCurrentSession()
-                .createCriteria(RecentCitationsEntity.class)
-                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
 
-        return listUser;
+//        List<RecentCitationsEntity> listUser = (List<RecentCitationsEntity>) sessionFactory.getCurrentSession()
+//                .createCriteria(RecentCitationsEntity.class)
+//                .setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY).list();
+        return (List<RecentCitationsEntity>) sessionFactory.getCurrentSession()
+                .createCriteria(RecentCitationsEntity.class).add(Restrictions.eq("PMID", PMIDs)).list();
+
     }
-
-
-    /*
-    @Bean
-    public DataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://mysql.chpc.utah.edu:3306");
-        dataSource.setUsername("semmed");
-        dataSource.setPassword("mahdifatemeh");
-
-        return dataSource;
-    }
-
-    @Transactional
-    public List<String> getCitations(List<String> PMIDs) {
-        String queryCitations = "SELECT Citationjson FROM SemanticMedline.RECENT_CITATIONS WHERE PMID in (';";
-        long count = 1;
-        for (String s: PMIDs) {
-            queryCitations += s;
-            if(count != PMIDs.size()) {
-                queryCitations += ", ";
-            }
-            count++;
-        }
-        queryCitations += "');";
-
-        ContactDAOImpl(getDataSource());
-
-
-        List<String> data = jdbcTemplate.query(queryCitations, new RowMapper<String>(){
-            public String mapRow(ResultSet rs, int rowNum)
-                    throws SQLException {
-                return rs.getString(1);
-            }
-        });
-
-
-        return data;
-    }
-    */
 }
