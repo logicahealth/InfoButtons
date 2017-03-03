d1d5f5cacd790cb1899914508dda171714654370
Andrew Iskander <andrew.iskander@utah.edu>
2017-03-03 08:57:38 -0700

    Added a database and default LITE admin user to user table creation script

--- DeploymentPackage/dbScripts/userAuthentication.sql
+++ DeploymentPackage/dbScripts/userAuthentication.sql
@@ -1,6 +1,10 @@
+USE `resource_profile`;
+
 CREATE TABLE users
 (
     username VARCHAR(45) PRIMARY KEY NOT NULL,
     password VARCHAR(45) NOT NULL,
     role VARCHAR(12)
 );
+
+INSERT INTO `users` (`username`, `password`, `role`) VALUES ('root', 'password', 'ADMIN');
