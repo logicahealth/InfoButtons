823238ff1a11177897dd155ce8bdcefc5bfbf6f2
Andrew Iskander <shrikeg@gmail.com>
2017-05-06 13:37:49 -0600

    Scoping response generator to current session

--- oib-request/oib-valueset-matcher/src/test/java/edu/utah/openinfobutton/valuset/matcher/impl/cloud/CloudValueSetMatcherImplTest.java
+++ oib-request/oib-valueset-matcher/src/test/java/edu/utah/openinfobutton/valuset/matcher/impl/cloud/CloudValueSetMatcherImplTest.java
@@ -3,12 +3,8 @@ package edu.utah.openinfobutton.valuset.matcher.impl.cloud;
 import edu.utah.openinfobutton.valuset.matcher.api.ValueSetMatcher;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.*;
 
@@ -17,17 +13,7 @@ import static org.junit.Assert.*;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {  "/testContext.xml"})
-@WebAppConfiguration
 public class CloudValueSetMatcherImplTest {
-
-    @Autowired
-    WebApplicationContext wac;
-
-    @Autowired
-    MockHttpServletRequest request;
-
-    @Autowired
-    MockHttpSession session;
 
     @Autowired
     ValueSetMatcher matcher;
