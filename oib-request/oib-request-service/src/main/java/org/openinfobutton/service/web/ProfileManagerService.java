16d2d289970e07e79ffba2869d8c1deaa3380925
Andrew Iskander <andrew.iskander@utah.edu>
2017-05-08 10:01:03 -0600

    Removing separate HttpServlet instance for OIB request manager and using Spring MVC dispatcher servlet instead.

--- oib-request/oib-request-service/src/main/java/org/openinfobutton/service/web/ProfileManagerService.java
+++ oib-request/oib-request-service/src/main/java/org/openinfobutton/service/web/ProfileManagerService.java
@@ -24,7 +24,6 @@ import org.openinfobutton.schemas.kb.KnowledgeResourceProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import org.openinfobutton.liteprofiledb.domain.CloudProfiles;
@@ -48,7 +47,8 @@ import javax.xml.bind.Unmarshaller;
  * @author Andrew Iskander {@code <andrew.iskander@utah.edu>}
  * @version March 13, 2016
  */
-@Controller
+@RestController
+@RequestMapping("liteManager")
 public class ProfileManagerService
 {
 
