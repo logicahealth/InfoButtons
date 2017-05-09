6af2bcff5ad2db1af85e9f05664f4252e4c419b7
Andrew Iskander <andrew.iskander@utah.edu>
2017-05-09 13:09:54 -0600

    Fixed sql formatting

--- DeploymentPackage/dbScripts/valueset_and_log.sql
+++ DeploymentPackage/dbScripts/valueset_and_log.sql
@@ -20,12 +20,12 @@ USE `valueset_and_log`;
 DROP TABLE IF EXISTS `subset_json`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE subset_json
+CREATE TABLE `valueset_and_log`.`subset_json`
 (
-  id INT(11) PRIMARY KEY NOT NULL,
-  last_updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  name VARCHAR(255) NOT NULL,
-  value_set LONGBLOB
+  `id` INT(11) PRIMARY KEY NOT NULL,
+  `last_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` VARCHAR(255) NOT NULL,
+  `value_set` LONGBLOB
 )ENGINE=MyISAM DEFAULT CHARSET=latin1
 /*!40101 SET character_set_client = @saved_cs_client */;
 
