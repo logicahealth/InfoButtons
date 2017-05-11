ee0af76696f0c124e7826822cc890eefd138ea0f
Andrew Iskander <andrew.iskander@utah.edu>
2017-05-11 10:51:05 -0600

    Fixing database creation scripts

--- DeploymentPackage/dbScripts/valueset_and_log.sql
+++ DeploymentPackage/dbScripts/valueset_and_log.sql
@@ -20,7 +20,7 @@ USE `valueset_and_log`;
 DROP TABLE IF EXISTS `subset_json`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `valueset_and_log`.`subset_json`
+CREATE TABLE `subset_json`
 (
   `id` INT(11) PRIMARY KEY NOT NULL,
   `last_updated` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -38,5 +38,6 @@ CREATE TABLE `logs` (
   `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
   `clientIP` varchar(45) NOT NULL,
   `orgID` varchar(45) NOT NULL,
+  `response` mediumtext NULL
   PRIMARY KEY  (`logsId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=106 DEFAULT CHARSET=latin1;
