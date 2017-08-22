02b4159554190f1496f7b2403ce424ba1f659953
aniskand <WND-r3700>
2017-08-22 08:00:00 -0600

    OIB property database initial commit

---
+++ oib-request/oib-request-db/src/main/java/org/openinfobutton/oibpropertydb/domain/OibAppProperty.java
@@ -0,0 +1,77 @@
+package org.openinfobutton.oibpropertydb.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "oib_app_property", schema = "resource_profile", catalog = "")
+public class OibAppProperty {
+    private int appPropertyId;
+    private String propName;
+    private String propDescription;
+    private String propValue;
+
+    @Id
+    @Column(name = "app_property_id")
+    public int getAppPropertyId() {
+        return appPropertyId;
+    }
+
+    public void setAppPropertyId(int appPropertyId) {
+        this.appPropertyId = appPropertyId;
+    }
+
+    @Basic
+    @Column(name = "prop_name")
+    public String getPropName() {
+        return propName;
+    }
+
+    public void setPropName(String propName) {
+        this.propName = propName;
+    }
+
+    @Basic
+    @Column(name = "prop_description")
+    public String getPropDescription() {
+        return propDescription;
+    }
+
+    public void setPropDescription(String propDescription) {
+        this.propDescription = propDescription;
+    }
+
+    @Basic
+    @Column(name = "prop_value")
+    public String getPropValue() {
+        return propValue;
+    }
+
+    public void setPropValue(String propValue) {
+        this.propValue = propValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OibAppProperty that = (OibAppProperty) o;
+
+        if (appPropertyId != that.appPropertyId) return false;
+        if (propName != null ? !propName.equals(that.propName) : that.propName != null) return false;
+        if (propDescription != null ? !propDescription.equals(that.propDescription) : that.propDescription != null)
+            return false;
+        if (propValue != null ? !propValue.equals(that.propValue) : that.propValue != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = appPropertyId;
+        result = 31 * result + (propName != null ? propName.hashCode() : 0);
+        result = 31 * result + (propDescription != null ? propDescription.hashCode() : 0);
+        result = 31 * result + (propValue != null ? propValue.hashCode() : 0);
+        return result;
+    }
+}
