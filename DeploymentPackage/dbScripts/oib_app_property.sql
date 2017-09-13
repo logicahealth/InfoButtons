3db9d5afa84cfb0398cbe4f23511cbbb0b4539c4
aniskand <WND-r3700>
2017-09-13 16:06:06 -0600

    Adding auto_increment to primary key

--- DeploymentPackage/dbScripts/oib_app_property.sql
+++ DeploymentPackage/dbScripts/oib_app_property.sql
@@ -1,7 +1,7 @@
 create table oib_app_property
 (
 	app_property_id int not null
-		primary key,
+		auto_increment, primary key,
 	prop_name varchar(50) not null,
 	prop_description varchar(50) null,
 	prop_value varchar(50) not null
