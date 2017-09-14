e5b7f056c83d3da0ed5c23c6ea5383df168d31af
Andrew Iskander <andrew.iskander@utah.edu>
2017-09-14 08:04:04 -0600

    Cleaning up some code comments

--- oib-request/oib-request-service/src/main/java/org/openinfobutton/service/configuration/OibPropertiesConfig.java
+++ oib-request/oib-request-service/src/main/java/org/openinfobutton/service/configuration/OibPropertiesConfig.java
@@ -57,15 +57,12 @@ public class OibPropertiesConfig {
 
             Configuration databaseConfiguration = builder.getConfiguration();
 
-            //CommonsConfigurationFactoryBean comes from https://java.net/projects/springmodules/sources/svn/content/tags/release-0_8/projects/commons/src/java/org/springmodules/commons/configuration/CommonsConfigurationFactoryBean.java?rev=2110
-            //Per https://jira.spring.io/browse/SPR-10213 I chose to just copy the raw source into our project
             ConfigurationPropertiesFactoryBean commonsConfigurationFactoryBean = new ConfigurationPropertiesFactoryBean(databaseConfiguration);
 
             Properties dbProps = commonsConfigurationFactoryBean.getObject();
             PropertiesPropertySource dbPropertySource = new PropertiesPropertySource("dbPropertySource", dbProps);
 
             //By being First, Database Properties take precedence over all other properties that have the same key name
-            //You could put this last, or just in front of the application.properties if you wanted to...
             propertySources.addFirst(dbPropertySource);
         } catch (Exception e) {
             log.error("Error during database properties setup", e);
@@ -73,10 +70,6 @@ public class OibPropertiesConfig {
         }
     }
 
-    /**
-     * Read why this is required: http://www.baeldung.com/2012/02/06/properties-with-spring/#java
-     * It is important to be static: http://www.java-allandsundry.com/2013/07/spring-bean-and-propertyplaceholderconf.html
-     */
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
