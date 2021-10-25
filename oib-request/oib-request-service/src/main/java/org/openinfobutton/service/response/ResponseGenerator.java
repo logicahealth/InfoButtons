7f07875beab16760b8a3bfbf7a65b50d7ca2624f
Emerson Borsato <emerson.borsato@utah.edu>
2021-10-25 09:37:16 -0600

    Fixed small dependency bug.

--- oib-request/oib-request-service/src/main/java/org/openinfobutton/service/response/ResponseGenerator.java
+++ oib-request/oib-request-service/src/main/java/org/openinfobutton/service/response/ResponseGenerator.java
@@ -73,8 +73,10 @@ import org.openinfobutton.service.matching.RequestResult;
 import org.openinfobutton.service.transform.TransformCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Component;
 
 @Configurable( preConstruction = true )
+@Component
 public class ResponseGenerator
 {
 
